42b0cc089f4b14460871e41bbbbb56a5de01c64f
p752221 <viet.c.ma@nab.com.au>
2021-02-19 12:14:20 +1100

    adding Makefile

--- scripts/install-jenkins-plugins.sh
+++ scripts/install-jenkins-plugins.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 set -o errexit
 
-for config_file in $( find -L "../configs/" -maxdepth 1 -type f -a \( -name '*.yaml' -o -name '*.yml' \) | sort ); do
+for config_file in $( find -L "configs/" -maxdepth 1 -type f -a \( -name '*.yaml' -o -name '*.yml' \) | sort ); do
   extra_vars+=( --extra-vars "@${config_file}" )
 done
 
 echo "Installing Jenkins plugins ..."
 
-ANSIBLE_CONFIG=../ansible/configs/ansible.cfg \
-  ANSIBLE_LIBRARY=../ansible/library/ \
-  ansible-playbook "../ansible/playbooks/install-jenkins-plugins.yaml" \
+ANSIBLE_CONFIG=ansible/configs/ansible.cfg \
+  ANSIBLE_LIBRARY=ansible/library/ \
+  ansible-playbook "ansible/playbooks/install-jenkins-plugins.yaml" \
   "${extra_vars[@]}"
 
 echo "Finished installing Jenkins plugins ..."
